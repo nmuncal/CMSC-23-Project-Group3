8188cbff6970124d5e9fde64f7f9f857890dbf92
Axel Balitaan <balitaanaxel@gmail.com>
2024-05-22 16:33:26 +0800

    chore: fixed pending organization pages, adjusted firebase auth according to new model

--- lib/pages/views/admin/admin_profile.dart
+++ lib/pages/views/admin/admin_profile.dart
@@ -1,4 +1,6 @@
+import 'package:cmsc_23_project_group3/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AdminProfile extends StatefulWidget {
   const AdminProfile({super.key});
@@ -10,6 +12,12 @@ class AdminProfile extends StatefulWidget {
 class _AdminProfileState extends State<AdminProfile> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child:Text("Admin Profile"));
+    return  ElevatedButton(
+              onPressed:  () {
+                context.read<UserAuthProvider>().signOut();
+              },
+
+              child: Text('Sign Out'),
+            );
   }
 }
